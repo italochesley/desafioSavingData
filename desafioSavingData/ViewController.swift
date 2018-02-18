4f6136955625fc1fc9d86ddaa7b64ef9dbd11adb
Ítalo Chesley <igomes@ciandt.com>
2018-02-18 10:59:09 -0300

    adicionando solução de userDefaults

--- desafioSavingData/ViewController.swift
+++ desafioSavingData/ViewController.swift
@@ -12,14 +12,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        incrementarContadorUtilizacoesApp()
+        if deveSolicitarAvaliacao() {
+            zerarContadorUtilizacoesAPP()
+            let alert = UIAlertController(title: "Avaliação", message: "Está gostando da app? Então clique em Avaliar para ir a loja da Apple e fazer uma avaliação", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Avaliar", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func deveSolicitarAvaliacao() -> Bool {
+        
+        let userDefaults = UserDefaults.standard
+        let contadorUtilizacoes = userDefaults.double(forKey: "contadorUtilizacoes")
+        return contadorUtilizacoes >= 2
     }
-
     
+    func zerarContadorUtilizacoesAPP() {
+        let userDefaults = UserDefaults.standard
+        userDefaults.set(0, forKey: "contadorUtilizacoes")
+    }
+    
+    func incrementarContadorUtilizacoesApp() {
+        let userDefaults = UserDefaults.standard
+        var contadorUtilizacoes = userDefaults.double(forKey: "contadorUtilizacoes")
+        contadorUtilizacoes += 1
+        userDefaults.set(contadorUtilizacoes, forKey: "contadorUtilizacoes")
+    }
 }
 
